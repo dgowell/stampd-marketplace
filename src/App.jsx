76d225f6935d21cf2ccaded667a8069ab3328ff3
Joel Cormack <dgowellkomrak@gmail.com>
2022-10-31 10:51:01 -0600

    After installing packages

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,13 @@
-import { useEffect } from "react";
 import minimaLogo from './minima_logo.png';
 import './App.css';
 
 function App() {
-  useEffect(() => {
-      window.MDS.init();
-  }, []);
-
   return (
     <div className="App">
       <section className="container">
         <img src={minimaLogo} className="logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code>.
+          Edit <code>src/App.jsx</code>.
         </p>
       </section>
     </div>
